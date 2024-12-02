86751439b4f3e3b9c1e8a1a23dc10646781dd61c
Malte Zerr <m.sauerwein@live.de>
2024-12-02 21:54:18 +0100

    update audio file import script

--- src/cardsync/input/import_audiofiles.sh
+++ src/cardsync/input/import_audiofiles.sh
@@ -4,4 +4,7 @@ output="out.csv"
 sep=","
 q=\"\"
 echo "title"$sep"artist"$sep"album"$sep"filename"$sep"duration" >"$output"
-find "$AUDIO_DIR" -type f -name "*.mp3" -print0 | xargs -0 -I {} mediainfo --Inform="General;$q%Title%$q,$q%Performer%$q,$q%Album%$q,$q%FileNameExtension%$q,%Duration%" {} >>"$output"
+find "$AUDIO_DIR" -type f -name "*.mp3" -print0 |
+xargs -0 -I {} mediainfo --Inform="General;$q%Title%$q$sep$q%Performer%$q$sep$q%Album%$q$sep$q%FileNameExtension%$q$sep%Duration%" {} |
+sed 's/""//g' >> "$output"
+
