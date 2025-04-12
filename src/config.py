7caafb9792d003fc098cb75ae545587daaa0ee10
Malte Zerr <m.sauerwein@live.de>
2025-04-13 01:31:28 +0200

    codecov

--- src/config.py
+++ src/config.py
@@ -66,9 +66,6 @@ class TestingConfig(Config):
     TESTING = True
     TONITUNES_SONGS_DIR = os.path.join(ROOTDIR, "tests/app/resources", "songs")
     TONITUNES_CARDS_DIR = os.path.join(ROOTDIR, "tests/app/resources", "cards")
-    # DATABASE_URI = "sqlite:///" + os.path.join(
-    #     ROOTDIR, "tests/app/resources/sqlite/test-data.sqlite"
-    # )
     DATABASE_URI = "sqlite:///:memory:"
 
 
